83e119860ca6ada2c36fbef8c6de9821ecc2aaa2
Tom Ari <freak.tom3@gmail.com>
2022-06-21 16:57:14 +0300

    test case indicator

--- src/components/visual/Controls.js
+++ src/components/visual/Controls.js
@@ -12,29 +12,68 @@ export const Controls = ({ testCases, testCase, setTestCase }) => {
     setTestCase(testCase - 1);
   };
 
+  const onChange = (e) => {
+    if (
+      e.target.value <= 0 ||
+      e.target.value > testCases.length ||
+      !e.target.value
+    )
+      return;
+    setTestCase(parseInt(e.target.value) - 1);
+  };
+
   return (
-    <div className="columns">
-      <div className="column">
-        <button className="button">prev</button>
-        <button className="button">pause</button>
-        <button className="button">play</button>
-        <button className="button">next</button>
+    <div className="level">
+      <div className="level-left">
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">player</p>
+            <div className="level">
+              <div className="level-item">
+                <button className="button">prev</button>
+              </div>
+              <div className="level-item">
+                <button className="button">pause</button>
+              </div>
+              <div className="level-item">
+                <button className="button">play</button>
+              </div>
+              <div className="level-item">
+                <button className="button">next</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="column">
-        <button className="button" onClick={prevTestCase}>
-          prev test case
-        </button>
-        <button className="button" onClick={nextTestCase}>
-          next test case
-        </button>
-        <label>
-          <input
-            className="input no-arrows"
-            type="number"
-            min={1}
-            max={testCases.length}
-          />
-        </label>
+
+      <div className="level-right">
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">test cases</p>
+            <div className="level">
+              <div className="level-item">
+                <button className="button" onClick={prevTestCase}>
+                  previous
+                </button>
+              </div>
+              <div className="level-item">
+                <button className="button" onClick={nextTestCase}>
+                  next
+                </button>
+              </div>
+              <div className="level-item">
+                <input
+                  className="input has-no-arrows is-wide"
+                  type="number"
+                  min={1}
+                  max={testCases.length}
+                  placeholder={`${parseInt(testCase) + 1}/${testCases.length}`}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/visual/Player.js
+++ src/components/visual/Player.js
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 import { Controls } from "./Controls";
 import "../../pages/styles.scss";
 
-export const Player = ({ func, testCases }) => {
+export const Player = ({ title, func, testCases }) => {
   const [testCase, setTestCase] = useState(0);
+
+  const output = func(testCases[testCase]);
 
   return (
     <section className="section">
+      <p className="heading">{title}</p>
       <div className="box has-background-light">
         <div className="section">
           <div className="block">
-            test case:
+            input:
             <span className="has-text-weight-bold"> {testCases[testCase]}</span>
           </div>
+
           <div className="block">
-            input: <span className="has-text-weight-bold"></span>
-          </div>
-          <div className="block">
-            output: <span className="has-text-weight-bold"></span>
+            output:
+            <span className="has-text-weight-bold"> {output.toString()}</span>
           </div>
         </div>
         <Controls
